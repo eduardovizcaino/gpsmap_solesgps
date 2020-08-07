28dcc77fdbee595df78fdef78d7ea0e8569851bf
Eduardo Vizcaino <evigra@hotmail.com>
2020-08-07 12:24:43 -0500

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -173,11 +173,6 @@ odoo.define('gpsmap', function(require){
                                 vehiculo["im"]=vehiculo_img;
                                 
 	                            locationsMap(vehiculo);            
-	                            
-                                if($("li.vehicle[vehicle='"+device_id+"']").length>0)                        
-                                    $("li.vehicle[vehicle='"+device_id+"']").attr(vehiculo);
-
-	                            
 	                            if(device_active==device_id) execute_streetMap(vehiculo);
                             }    
                         }
