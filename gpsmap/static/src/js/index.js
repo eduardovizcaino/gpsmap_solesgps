0f551cd9e58075537af4277da6c12bd0d2aab151
Eduardo Vizcaino <evigra@hotmail.com>
2020-08-20 10:01:17 -0500

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -394,8 +394,7 @@ odoo.define('gpsmap', function(require){
     			                    $(this).addClass(\"vehicle_active\");\
     			                    device_active               =$(this).attr(\"vehicle\");\
     			                    if(tipo==\"gpsmap.menu_gpsmap_maphistory\"){\
-    			                        $(\"div#filtro\").css(\"display\", \"auto\");\
-    			                        alert(tipo);\
+    			                        $(\"div#filtro\").css(\"display\", \"contents\");\
     			                    }else{\
     			                        $(\"div#filtro\").css(\"display\", \"none\");\
                                         status_device(this);\
