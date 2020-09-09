e876aea2f9bd40e391f6b39dfc4fdbb685e95335
Eduardo Vizcaino <evigra@hotmail.com>
2020-09-09 01:10:19 -0500

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -156,7 +156,7 @@ class positions(models.Model):
         
         if len(vehicle_data)>0:         
             for vehicle in vehicle_data:
-                print("VEHICULO JS POSITION=== ", vehicle.positionid.servertime)
+                print("VEHICULO JS POSITION=== ", vehicle.positionid)
                 positions_arg                   =[('deviceid','=',vehicle.id)]                
                 positions_data                  =self.search_read(positions_arg, offset=0, limit=1, order='devicetime DESC')        
                 if len(positions_data)>0:                            
