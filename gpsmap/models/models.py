b60920f1a53668d57e29b68e125eea0311f9d852
[SolesGPS] Eduardo Vizcaino <evigra@hotmail.com>
2020-10-20 11:25:42 -0500

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -309,6 +309,19 @@ class positions(models.Model):
                 position["leido"]                           =1                                
                 positions_obj.write(position)
                 vehicle_obj.write(vehicle)
+class route(models.Model):
+    _name = "gpsmap.route"
+    _description = 'GPS Route'
+    _pointOnVertex=""
+    name = fields.Char('Name', size=75)
+    description = fields.Char('Description', size=150)
+    area = fields.Text('area')
+    attributes = fields.Text('Attributes')
+    points = fields.Text('Points')
+    hidden = fields.Boolean('Hidden')
+    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, required=True)
+
+
 class geofence(models.Model):
     _name = "gpsmap.geofence"
     _description = 'GPS Geofence'
