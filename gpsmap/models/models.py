108041003868c2caefae502ede5a73dcf6816d97
[SolesGPS] Eduardo Vizcaino <evigra@hotmail.com>
2020-10-26 14:43:58 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -321,6 +321,7 @@ class route(models.Model):
     points = fields.Text('Points')
     hidden = fields.Boolean('Hidden')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, required=True)
+    company_ids = fields.Many2many('res.company', 'route_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
 
 
 class geofence(models.Model):
@@ -370,3 +371,4 @@ class geofence_device(models.Model):
     geofence_ids = fields.Many2many('gpsmap.geofence', 'alert_geofence', 'geofence_id', 'alert_id', string='Geofence')
     device_ids = fields.Many2many('fleet.vehicle', 'alert_device', 'device_id', 'alert_id', string='Device')            
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, required=True)
+    company_ids = fields.Many2many('res.company', 'geofence_device_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
