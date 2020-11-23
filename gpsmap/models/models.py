907dd419c2320a1fcc483ec172a6f714731d5c23
[SolesGPS] Eduardo Vizcaino <evigra@hotmail.com>
2020-11-22 19:04:59 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -159,7 +159,7 @@ class vehicle(models.Model):
                 #if vehicle.gps1_id.positionid &gt; 0: 
                 if len(vehicle.gps1_id.positionid)>0:                 
                     print("==== gps1_ ID=== ", vehicle.gps1_id.positionid.id)
-                    print("==== gps1_ LONGITUDE=== ", vehicle.gps1_id.positionid.mapped(["longitude"]))
+                    print("==== gps1_ LONGITUDE=== ", vehicle.gps1_id.positionid.read(["longitude"])[0])
                     
                     
                     #print("==== DATA ID=== ", vehicle.gps1_id.positionid)
