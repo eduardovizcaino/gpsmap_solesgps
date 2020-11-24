9f87d56238b49d321e300b36d176dddfc1447473
[SolesGPS] Eduardo Vizcaino <evigra@hotmail.com>
2020-11-24 07:16:40 -0600

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -400,7 +400,7 @@ odoo.define('gpsmap', function(require){
 		            for(ivehiculos in vehiculos)
 		            {		                
 		                var vehiculo        =vehiculos[ivehiculos];		                
-                        var vehiculo_id     =vehiculo["gps1_id"];
+                        var vehiculo_id     =vehiculo["gps1_id"]["id"];
                         var vehiculo_name   =vehiculo["name"].split("/");;
                         vehiculo_name       =vehiculo_name[0]
                         
