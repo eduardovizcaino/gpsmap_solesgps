44baa5e554f171d62dc74006ed998aa11dec3a86
Eduardo Vizcaino <evigra@hotmail.com>
2021-01-24 09:54:36 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -70,10 +70,10 @@ class tc_positions(models.Model):
     	print("SELF######", self)
     	return_positions                        ={}
     	
-    	if(self[0]>0):
-    		print("VEHICULO######", self)
+    	#if(self[0]>0):
+    	#	print("VEHICULO######", self)
     	
-        vehicle_obj                             =self.env['fleet.vehicle']        
+        #vehicle_obj                             =self.env['fleet.vehicle']        
         #vehicle_args                            =[('deviceid','=',vehicle.id)]        
         
         #vehicle_data                            =vehicle_obj.search(vehicle_args, offset=0, limit=None, order=None)
