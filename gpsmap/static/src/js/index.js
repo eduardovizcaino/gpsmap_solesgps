506f33b844fcb0609f7691ade90a4cec60f98f9c
Eduardo Vizcaino <evigra@hotmail.com>
2021-01-24 10:03:26 -0600

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -249,7 +249,7 @@ odoo.define('gpsmap', function(require){
                     method: "js_positions",
                     fields: fields_select,
                     order:  "devicetime DESC",           
-                    domain: [[("devicetime",">",start_time)]]                                
+                    domain: [("devicetime",">",start_time)]                                
                 };                  
                 
                 //if(device_active!=0)                
