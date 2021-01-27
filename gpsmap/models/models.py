7405a89cbbc7dc34eab90419ba82989794f9df17
Eduardo Vizcaino <evigra@hotmail.com>
2021-01-26 23:04:57 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -69,7 +69,6 @@ class tc_positions(models.Model):
 
     #@api.model
     def js_positions(self):
-    	print("aaaaa", self)
         vehicle_obj                             =self.env['fleet.vehicle']        
         vehicle_args                            =[]        
         return_positions                        ={}
