a79c9fedf791e55a3fd9bbbac2077531030dcfa1
Eduardo Vizcaino <evigra@hotmail.com>
2021-02-06 18:53:41 -0600

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -251,8 +251,8 @@ odoo.define('gpsmap', function(require){
                 model={   
                     model:  "fleet.vehicle",
                     method: "positions",
-                    args:[],
-                    kwargs:[]
+                    args:[]
+//                    kwargs:[]
                 };                  
 
 
