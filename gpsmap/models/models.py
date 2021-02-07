bfe5ca5ff60356122030fd75418d1728d5f48ad3
Eduardo Vizcaino <evigra@hotmail.com>
2021-02-07 13:49:47 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -77,7 +77,7 @@ class tc_positions(models.Model):
             ('type', 'in', ['out_invoice', 'out_refund'])
         ]
         """
-        #data = self.search_read(datas["data"]["domain"], datas["fields"])
+        data = self.search_read(datas["data"]["domain"], datas["fields"])
         
     	#print("#### DATA #######",data)
 
