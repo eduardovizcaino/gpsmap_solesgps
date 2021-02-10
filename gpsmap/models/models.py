f361b982a0b3b5ab09da1841095941eb928c9d89
Eduardo Vizcaino <evigra@hotmail.com>
2021-02-09 23:34:01 -0600

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -33,6 +33,7 @@ class vehicle_model_brand(models.Model):
 class tc_devices(models.Model):
     _name = "tc_devices"
     _description = 'traccar devices'
+    _order = "name DESC"
         
     name                                        = fields.Char('Name', size=128)
     uniqueid                                    = fields.Char('IMEI', size=128)
@@ -42,14 +43,14 @@ class tc_devices(models.Model):
     telcel                                      = fields.Boolean('Telcel', default=True)
     signal                                      = fields.Boolean('Good signal', default=True)
     
-    
+    """
 class tc_positions(models.Model):
     _name = "tc_positions"
     _description = 'traccar Positions'
-
+    _order = "devicetime DESC"
+    _pointOnVertex=""
     protocol                                    = fields.Char('Protocolo', size=15)
     #deviceid                                    = fields.Many2one('tc_devices',ondelete='set null', string="Vehiculo", index=True)
-    deviceid                                    = fields.Integer('GPS')
     servertime                                  = fields.Datetime('Server Time')
     devicetime                                  = fields.Datetime('Device Time')
     fixtime                                     = fields.Datetime('Error Time')
@@ -64,59 +65,9 @@ class tc_positions(models.Model):
     accuracy                                    = fields.Float('Curso',digits=(3,2))
     network                                     = fields.Char('Type', size=4000)
     read                                        = fields.Integer('Leido',default=0)
-    @api.multi
-    def positions(self,datas):		   
-        start_time  =datas["data"]["domain"][0][2]
-        end_time    =datas["data"]["domain"][1][2]       
-        deviceid    =datas["data"]["domain"][2][2]
-    
-    	
 
-        """
-        positions_data   = self.search(datas["data"]["domain"], offset=0, limit=None, order=None)    
-        for positions in positions_data:
-            print("#### DATA #######",positions)
-        """
+    """
 
-        #"SELECT id FROM tc_devices td WHERE td.uniqueid='%s' " %(self.imei)
-        sql="""
-            SELECT tp.*, tp.deviceid as tp_deviceid, td.phone,
-                CASE 		                
-                    WHEN fv.odometer_unit='kilometers' THEN 1.852 * tp.speed
-                    WHEN fv.odometer_unit='miles' THEN 1.15 * tp.speed
-                    ELSE 1.852 * tp.speed                    
-                END	AS speed_compu,
-                CASE 				            
-	                WHEN tp.attributes::json->>'alarm'!='' THEN tp.attributes::json->>'alarm'
-	                WHEN tp.attributes::json->>'motion'='false' THEN 'Stopped'
-	                WHEN tp.attributes::json->>'motion'='true' AND tp.speed>2 THEN 'Moving'
-	                ELSE 'Stopped'
-                END	as event,                                 
-                CASE 				            
-                    WHEN tp.attributes::json->>'alarm'!='' THEN 'alarm'
-                    WHEN now() between tp.devicetime - INTERVAL '15' MINUTE AND tp.devicetime + INTERVAL '15' MINUTE THEN 'Online'
-                    ELSE 'Offline'
-                END  as status
-            FROM  fleet_vehicle fv
-                join tc_devices td on fv.gps1_id=td.id
-                join tc_positions tp on td.id=tp.deviceid
-            WHERE  1=1          
-                AND tp.devicetime>'%s'
-                AND tp.devicetime<'%s'
-        """ %(start_time,end_time)
-        if int(deviceid)>0:
-            sql="%s AND td.id='%s' " %(sql,deviceid)
-               
-        self.env.cr.execute(sql)
-        return_positions                    =[]
-        positions                           =self.env.cr.dictfetchall()
-        for position in positions:
-            position["de"]            =position["tp_deviceid"]                            
-            tp_deviceid               =position["tp_deviceid"]
-                        
-            return_positions.append(position)
-            
-        return return_positions    
 
 class vehicle(models.Model):
     _inherit = "fleet.vehicle"
@@ -139,7 +90,7 @@ class vehicle(models.Model):
     economic_number                             = fields.Char('Economic Number', size=50)
     #imei                                        = fields.Char('Imei', size=50)
     speed                                       = fields.Char('Exceso de Velocidad', default=100, size=3)   
-    #positionid                                  = fields.Many2one('gpsmap.positions',ondelete='set null', string="Position", index=True)    
+    positionid                                  = fields.Many2one('gpsmap.positions',ondelete='set null', string="Position", index=True)    
     motor                                       = fields.Boolean('Motor', default=True, track_visibility="onchange")
     #devicetime                                  = fields.Datetime('Device Time')
     #devicetime_compu                            = fields.Datetime('Device Time', compute='_get_date')
@@ -155,49 +106,6 @@ class vehicle(models.Model):
             self.devicetime_compu=tz.localize(fields.Datetime.from_string(self.devicetime)).astimezone(pytz.utc)
         else:    
             self.devicetime_compu=self.devicetime
-    
-    @api.multi
-    def positions(self,data):		   
-        print("aaaaaaaaaaaa",data)
-    	
-    	
-    	
-    	
-    	
-    	
-    	    	
-        self.env.cr.execute("""
-            SELECT tp.*, tp.deviceid as tp_deviceid, td.phone,
-                CASE 		                
-                    WHEN fv.odometer_unit='kilometers' THEN 1.852 * tp.speed
-                    WHEN fv.odometer_unit='miles' THEN 1.15 * tp.speed
-                    ELSE 1.852 * tp.speed                    
-                END	AS speed_compu,
-                CASE 				            
-	                WHEN tp.attributes::json->>'alarm'!='' THEN tp.attributes::json->>'alarm'
-	                WHEN tp.attributes::json->>'motion'='false' THEN 'Stopped'
-	                WHEN tp.attributes::json->>'motion'='true' AND tp.speed>2 THEN 'Moving'
-	                ELSE 'Stopped'
-                END	as event,                                 
-                CASE 				            
-                    WHEN tp.attributes::json->>'alarm'!='' THEN 'alarm'
-                    WHEN now() between tp.devicetime - INTERVAL '15' MINUTE AND tp.devicetime + INTERVAL '15' MINUTE THEN 'Online'
-                    ELSE 'Offline'
-                END  as status
-            FROM  fleet_vehicle fv
-                join tc_devices td on fv.gps1_id=td.id
-                join tc_positions tp on td.positionid=tp.id
-        """)
-        
-        return_positions                    ={}
-        positions                           =self.env.cr.dictfetchall()
-        for position in positions:
-            position["de"]            =position["tp_deviceid"]                            
-            tp_deviceid               =position["tp_deviceid"]
-            
-            return_positions[tp_deviceid]    =position
-
-        return return_positions    
     def toggle_motor(self):
         try:
             sql="SELECT id FROM tc_devices td WHERE td.uniqueid='%s' " %(self.imei)    
@@ -276,16 +184,186 @@ class vehicle(models.Model):
 class speed(models.Model):
     _name = "gpsmap.speed"
     _description = 'Positions Speed'
-    #_order = "starttime DESC"
+    _order = "starttime DESC"
     deviceid                                    = fields.Many2one('fleet.vehicle',ondelete='set null', string="Vehiculo", index=True)
     starttime                                   = fields.Datetime('Start Time')
     endtime                                     = fields.Datetime('End Time')
     speed                                       = fields.Float('Velocidad',digits=(3,2))
     
     
+class positions(models.Model):
+    _name = "gpsmap.positions"
+    _description = 'GPS Positions'
+    _order = "devicetime DESC"
+    _pointOnVertex=""
+    protocol                                    = fields.Char('Protocolo', size=15)
+    deviceid                                    = fields.Many2one('fleet.vehicle',ondelete='set null', string="Vehiculo", index=True)
+    servertime                                  = fields.Datetime('Server Time')
+    devicetime                                  = fields.Datetime('Device Time')
+    devicetime_compu                            = fields.Datetime('Device Time', compute='_get_date')
+    fixtime                                     = fields.Datetime('Error Time')
+    valid                                       = fields.Integer('Valido')
+    latitude                                    = fields.Float('Latitud',digits=(5,10))
+    longitude                                   = fields.Float('Longitud',digits=(5,10))
+    altitude                                    = fields.Float('Altura',digits=(6,2))
+    speed                                       = fields.Float('Velocidad',digits=(3,2))
+    speed_compu                                 = fields.Float('Velocidad', compute='_get_speed', digits=(3,2))
+    #gas_compu                                   = fields.Float('Gas', compute='_get_gas', digits=(5,2))
+    gas                                         = fields.Float('Gas', digits=(5,2))
+    course                                      = fields.Float('Curso',digits=(3,2))
+    address                                     = fields.Char('Calle', size=150)
+    attributes                                  = fields.Char('Atributos', size=5000)
+    status                                      = fields.Char('Type', size=5000)
+    #status_compu                                = fields.Char('Type', compute='_get_status', size=5000)
+    leido                                       = fields.Integer('Leido',default=0)
+    event                                       = fields.Char('Evento', size=70)
+    online                                      = fields.Boolean('Online', default=True)
+    """
+    @api.one
+    def _get_status(self):    
+        tz = pytz.timezone(self.env.user.tz) if self.env.user.tz else pytz.utc                            
+        hoy=tz.localize(fields.Datetime.from_string(datetime.now())).astimezone(pytz.utc)
+
+        ahora = datetime.datetime.utcnow()
+        ayer = ahora - datetime.timedelta(days=1)
+
+        print("Hoy=",hoy, "  ahora=", ahora,"   ayer=",ayer)
+    """
+    @api.one
+    def _get_speed(self):    
+        vehicle_obj                             =self.env['fleet.vehicle']        
+        vehicle                                 =vehicle_obj.browse(self.deviceid.id)
+
+        if(vehicle.odometer_unit=="kilometers"):     ts=1.852
+        if(vehicle.odometer_unit=="miles"):          ts=1.15
+        else:                                        ts=1.852
+            
+        self.speed_compu=self.speed * ts        
+    @api.one
+    def _get_date(self):            
+        tz = pytz.timezone(self.env.user.tz) if self.env.user.tz else pytz.utc                            
+        self.devicetime_compu=tz.localize(fields.Datetime.from_string(self.devicetime)).astimezone(pytz.utc)
+        
+    def get_system_para(self):
+        para_value                              =self.env['ir.config_parameter'].get_param('gpsmap_key','')
+        return para_value
+    def action_addpositions(self):
+        self.run_scheduler()
+        
+    @api.model    
+    def js_positions(self):
+        vehicle_obj                             =self.env['fleet.vehicle']        
+        vehicle_args                            =[]        
+        return_positions                        ={}
+        vehicle_data                            =vehicle_obj.search(vehicle_args, offset=0, limit=None, order=None)
+        if len(vehicle_data)>0:         
+            for vehicle in vehicle_data:    
+
+                print("Anterior VEHICULO JS POSITION=== ", vehicle.positionid)
+                positions_arg                   =[('deviceid','=',vehicle.id)]                
+                positions_data                  =self.search_read(positions_arg, offset=0, limit=1, order='devicetime DESC')        
+                if len(positions_data)>0:                            
+                    return_positions[vehicle.id]    =positions_data[0]        
+            
+
+        return return_positions
+    def run_scheduler_del_position(self):
+        positions_arg                           =[('leido','=',0)]                
+        positions_data                          =self.search(positions_arg, offset=0, limit=1000, order='devicetime DESC')        
+
+    def run_scheduler_get_position(self):    
+        now                                     = datetime.datetime.now()
+                
+        positions_obj                           =self.env['gpsmap.positions']
+        vehicle_obj                             =self.env['fleet.vehicle']
+        speed_obj                               =self.env['gpsmap.speed']
+        #mail_obj                                =self.env['mail.message']
+        geofence_obj                            =self.env['gpsmap.geofence']
+                
+        alerts_data                             =geofence_obj.geofences()
+        
+        positions_arg                           =[('leido','!=',1)]                
+        positions_data                          =positions_obj.search(positions_arg, offset=0, limit=200, order='devicetime DESC')        
+        
+        
+        #if type(positions_data) is list and len(positions_data)>0:     
+        if len(positions_data)>0:
+            #print('=============== READ POSITIONS ===================',len(positions_data))  
+            for position in positions_data:
+                vehicle_arg                     =[('id','=',position.deviceid.id)]                
+                vehicle                         =vehicle_obj.search(vehicle_arg)        
+                
+                if len(vehicle)>0:                                                                     
+                    if(vehicle.positionid.id==False):
+                        vehicle["positionid"]=position.id
+                    elif(vehicle.positionid.devicetime < position.devicetime):
+                        vehicle["positionid"]=position.id
+                                        
+                    if vehicle.speed=='':
+                        vehicle.speed               =100
+                    if vehicle.speed==0:
+                        vehicle.speed               =100    
+
+                    speed_arg                       =[['deviceid','=',position.deviceid.id],['endtime','=',False]]                
+                    speed_data                      =speed_obj.search(speed_arg, offset=0, limit=50000)        
+                                                                                    
+                    if float(vehicle.speed) < float(position.speed_compu):
+                        position["event"]    ="speeding"
+                        position["status"]   ="alarm"
+                        if(len(speed_data)==0):
+                            speed                       ={}
+                            speed["deviceid"]           =position.deviceid.id
+                            speed["starttime"]          =position.devicetime
+                            speed["speed"]              =position.speed_compu
+                            speed_obj.create(speed)
+                            
+                            mail                        ={}
+                            mail["model"]               ="gpsmap.positions"        
+                            mail["res_id"]              =position.id                        
+                            mail["message_type"]        ="comment"                        
+                            mail["body"]                ="Contenido del mensaje %s" %(vehicle.name) 
+                            
+                            #ail_obj.create(mail)        
+                            print('Exceso de velocidad===================')
+                            print(mail)                                                
+                    else:
+                        if(len(speed_data)>0):
+                            speed                       ={}
+                            for speed in speed_data:
+                                speed["endtime"]        =position.devicetime
+                                speed_obj.write(speed)                        
+                                #print('Saliendo del exceso de velocidad')
+                        #if len(speed_data)>0:
+                    """                    
+                    if len(alerts_data)>0:                     
+                        for alerts in alerts_data:                        
+                            for devices in alerts.device_ids:                 
+                                if(position.deviceid.id==devices.id):
+                                    print('==',alerts.name)
+                                    print('===========position device id======',position.deviceid.id)                                   
+                                    print('===========alert device id======',devices.id)
+                                    for geofences in alerts.geofence_ids:                 
+                                        print('===========',geofences)                                
+                    """                                        
+                    attributes = json.loads(position.attributes)
+                    
+                    if("io3" in attributes):                    gas     =attributes["io3"]        
+                    elif("fuel" in attributes):                 gas     =attributes["fuel"]        
+                    elif("fuel1" in attributes):                gas     =attributes["fuel1"]        
+                    else:                                       gas     =0
+                    
+                    if("alarm" in attributes):                  
+                        position["event"]                       =attributes["alarm"]
+                        position["status"]                      ="alarm"
+                
+                    position["gas"]                             =gas
+                position["leido"]                           =1                                
+                positions_obj.write(position)
+                vehicle_obj.write(vehicle)
 class route(models.Model):
     _name = "gpsmap.route"
     _description = 'GPS Route'
+    _pointOnVertex=""
     name = fields.Char('Name', size=75)
     description = fields.Char('Description', size=150)
     area = fields.Text('area')
@@ -299,6 +377,7 @@ class route(models.Model):
 class geofence(models.Model):
     _name = "gpsmap.geofence"
     _description = 'GPS Geofence'
+    _pointOnVertex=""
     name = fields.Char('Name', size=75)
     description = fields.Char('Description', size=150)
     area = fields.Text('area')
@@ -334,6 +413,7 @@ class geofence(models.Model):
 class geofence_device(models.Model):
     _name = "gpsmap.geofence_device"
     _description = 'GPS Geofence Device'
+    _pointOnVertex=""
     name = fields.Char('Name', size=75)
     description = fields.Char('Description', size=150)
     mail_in = fields.Char('Mail In', size=150)
