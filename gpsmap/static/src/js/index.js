315516407e1d0f4dcc31581961a949ed7057f2a8
Eduardo Vizcaino <evigra@hotmail.com>
2021-03-31 05:56:22 -0600

    Update

--- gpsmap/static/src/js/index.js
+++ gpsmap/static/src/js/index.js
@@ -488,6 +488,70 @@ odoo.define('gpsmap', function(require){
             },50);
 		},
         //////////////////////////////////////////////////////////////
+        butons_simulation: function() {
+
+		    var butons_html=" \
+			    <font id=\"back\"> -- </font>\
+			    <font id=\"play\">Play</font>\
+			    <font id=\"pause\">Pause</font>\
+			    <font id=\"stop\">Stop</font>\
+			    <font id=\"next\"> ++ </font>\
+		    ";
+		    $("#tablero2").html(butons_html);
+		    $("#tablero").html("");
+	    
+	        $("#play").button({
+			    icons: {        primary: "ui-icon-play"		    },
+			    text: false
+		    })
+			.click(function()
+		    {	
+                if(local.positions.length>0)
+			    {
+			        simulation_action="play";
+			        del_locations();
+			        $("div#odometro").show();
+				    // paint_history(isimulacion, historyMap);
+			    } 
+		    });
+	        $("#pause").button({
+			    icons: {        primary: "ui-icon-pause"		    },
+			    text: false
+		    })
+		    .click(function()
+		    {			    
+		        simulation_action="pause";
+		    });
+		    
+	        $("#next").button({
+			    icons: {        primary: "ui-icon-seek-next"		    },
+			    text: false
+		    })
+		    .click(function()
+		    {
+			    if(simulation_time>=50)
+				    simulation_time=simulation_time-50;
+		    });
+	        $("#back").button({
+			    icons: {        primary: "ui-icon-seek-prev"		    },
+			    text: false
+		    })
+		    .click(function()
+		    {
+			    simulation_time=simulation_time+50;
+		    });				
+	        $("#stop").button({
+			    icons: {        primary: "ui-icon-stop"		    },
+			    text: false
+		    })
+		    .click(function()
+		    {
+			    isimulacion=1;
+			    simulation_action="stop";
+		    });		
+        },
+
+        //////////////////////////////////////////////////////////////
         position: function() {
             console.log("POSITION ========");
             setTimeout(function()
@@ -1328,81 +1392,6 @@ odoo.define('gpsmap', function(require){
 
 	function butons_simulation()
 	{
-		var butons_html=" \
-			<font id=\"back\"> -- </font>\
-			<font id=\"play\">Play</font>\
-			<font id=\"pause\">Pause</font>\
-			<font id=\"stop\">Stop</font>\
-			<font id=\"next\"> ++ </font>\
-		";
-		$("#tablero2").html(butons_html);
-		$("#tablero").html("");
-	
-	    $("#play").button({
-			icons: {
-				primary: "ui-icon-play"
-			},
-			text: false
-			})
-			.click(function()
-			{	
-                if(local.positions.length>0)
-				//if(localizaciones.length>0)                
-				{
-				    simulation_action="play";
-				    del_locations();
-				    $("div#odometro").show();
-					// paint_history(isimulacion, historyMap);
-				} 
-			}
-		);
-	    $("#pause").button({
-			icons: {
-				primary: "ui-icon-pause"
-			},
-			text: false
-			})
-			.click(function()
-			{			    
-			    simulation_action="pause";
-			}
-		);
-		
-	    $("#next").button({
-			icons: {
-				primary: "ui-icon-seek-next"
-			},
-			text: false
-			})
-			.click(function()
-			{
-				if(simulation_time>=50)
-					simulation_time=simulation_time-50;
-			}
-		);
-	    $("#back").button({
-			icons: {
-				primary: "ui-icon-seek-prev"
-			},
-			text: false
-			})
-			.click(function()
-			{
-				simulation_time=simulation_time+50;
-			}
-		);				
-	    $("#stop").button({
-			icons: {
-				primary: "ui-icon-stop"
-			},
-			text: false
-			})
-			.click(function()
-			{
-				isimulacion=1;
-				simulation_action="stop";
-			}
-		);		
 	
 	}
 	
