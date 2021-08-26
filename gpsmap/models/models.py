072b51cc847e5008e4570bc5040871b247898cec
Eduardo Vizcaino <evigra@hotmail.com>
2021-08-25 22:14:15 -0500

    Update

--- gpsmap/models/models.py
+++ gpsmap/models/models.py
@@ -29,6 +29,45 @@ class vehicle_model_brand(models.Model):
 
 class odometer(models.Model):
     _inherit = "fleet.vehicle.odometer"
+    
+    activeTime                                       = fields.Float('Active Time',digits=(3,2))
+    
+    @api.model    
+    def run_scheduler_set_odometer(self):    
+        print("PASAS")
+    
+        self.env.cr.execute("""
+
+SELECT  vehicle_id,deviceid,date_trunc('day', fecha) as fecha,  ROUND(count(fecha)/60::numeric,2) as horas, round(max(distance)::numeric / 1000,3) as km
+FROM ( 
+	SELECT fv.id as vehicle_id, tp.deviceid, date_trunc('minute', tp.devicetime) as fecha,  max(tp.attributes::json->>'totalDistance') as distance
+	FROM tc_positions tp JOIN fleet_vehicle fv on fv.gps1_id=tp.deviceid
+	WHERE tp.attributes::json->>'motion'='true' AND tp.speed>2 	
+	AND ( date_trunc('day', now())=date_trunc('day', tp.devicetime)
+		 OR
+		 	date_trunc('day', tp.devicetime)='2021-08-24 00:00:00'
+		 )
+	GROUP BY tp.deviceid, date_trunc('minute', tp.devicetime),fv.id
+	ORDER BY date_trunc('minute', tp.devicetime) DESC
+) tabla
+GROUP BY vehicle_id,deviceid, date_trunc('day', fecha)
+ORDER BY date_trunc('day', fecha) DESC       
+
+""")
+        positions                           =self.env.cr.dictfetchall()
+        
+        for position in positions:
+            odometer_data                     ={}
+            
+            odometer_data["vehicle_id"]     =position["vehicle_id"]
+            odometer_data["date"]           =position["fecha"]
+            odometer_data["value"]          =position["km"]
+            odometer_data["activeTime"]          =position["horas"]
+            
+            self.create(odometer_data)            
+                
+            print("Device==",position["deviceid"]," fecha==",position["fecha"]," horas===",position["horas"]," km==",position["km"])
+
 
 class tc_devices(models.Model):
     _name = "tc_devices"
